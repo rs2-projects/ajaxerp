194e232092d34dc811cdf163b510981d6c705ca5
Md Riyad Hossein <riyadhossein@gmail.com>
2024-06-12 15:44:46 +0600

    product material category design fix

--- resources/views/inventory/product-material-category/_index_filtered.blade.php
+++ resources/views/inventory/product-material-category/_index_filtered.blade.php
@@ -22,7 +22,7 @@
                     <td class="erp-tbody-td text-center">{{ $data->name??'N/A' }}</td>
                     <td class="erp-tbody-td text-center">{{ $data->srp_markup_percent??'N/A' }}</td>
                     <td class="erp-tbody-td text-center">{{ $data->wholesale_discount_percent??'0' }}</td>
-                    <td class="erp-tbody-td text-center">{{ $data->description??'N/A' }}</td>
+                    <td class="erp-tbody-td text-center" title="{{ $data->description??'N/A' }}" style="max-width: 200px; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;">{{ $data->description??'N/A' }}</td>
                     @if(hasPermission('manage-product-material-category'))
                         <td class="text-end erp-tbody-td">
                             <div class="erp-action-t">
