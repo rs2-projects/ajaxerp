b192e7876ab88d115d9b74335ef513f5f9305e73
Abu Bin Oalid <oalid.mmj@gmail.com>
2024-11-07 15:32:04 +0600

    up

--- resources/views/production-staff/production/_receive.blade.php
+++ resources/views/production-staff/production/_receive.blade.php
@@ -250,7 +250,7 @@
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary" v-on:click="selectReceiveItem()">Deliver</button>
+                    <button type="button" class="btn btn-primary" v-on:click="selectReceiveItem()">Receive</button>
                 </div>
             </div>
         </div>
